234dd0b086805fe84b5582e2a9453dc4b02c95e0
Magarevedant <92874562+Magarevedant@users.noreply.github.com>
2026-04-21 13:22:32 +0530

    Rename anonymous function to login and invoke it

--- api/auth.mjs
+++ api/auth.mjs
@@ -2,11 +2,13 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 const supabase = createClient('https://unyomucsmtbxdqawvana.supabase.co', 'sb_publishable_buDnymqn0L6RVX0ORpTwKg_a2AaHjVz')
 
-async function () {
+async function login() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'github',
     options: {
     redirectTo: `http://localhost:8158/index.html`,
   },
   })
-}()
+}
+
+login()
