daebc1f8bf1c95b668944a483b0eefc17c478ece
Magarevedant <92874562+Magarevedant@users.noreply.github.com>
2026-04-21 13:34:47 +0530

    Refactor login function to API handler for OAuth

--- api/auth.mjs
+++ api/auth.mjs
@@ -2,13 +2,20 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 const supabase = createClient('https://unyomucsmtbxdqawvana.supabase.co', 'sb_publishable_buDnymqn0L6RVX0ORpTwKg_a2AaHjVz')
 
-async function login() {
+export default async function handler(req, res) {
+
+
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'github',
     options: {
-    redirectTo: `http://localhost:8158/index.html`,
-  },
-  })
-}
+      // Point this to your other Vercel function that handles the code
+      redirectTo: `https://velora-cyan-kappa.vercel.app/`,
+      skipBrowserRedirect: true // This gives us the URL instead of redirecting
+    },
+  });
 
-login()
+  if (error) return res.status(500).json({ error: error.message });
+
+  // Send the user to the GitHub Authorization page
+  return res.redirect(data.url);
+}
